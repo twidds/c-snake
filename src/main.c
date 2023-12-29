079084b664e26f7cbe6d333a2890880272f34ea6
Mark <>
2023-12-29 04:10:36 -0800

    playing with C input/output and compiler O level

--- src/main.c
+++ src/main.c
@@ -1,12 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <malloc.h>
+//#include <stdarg.h>
+
+
+//#define WIN32_LEAN_AND_MEAN
+//#include <windows.h>
+
+void setconsole(int w, int h);
+
+void clearconsole();
+
+void writeconsole(const char* data, int length);
 
 int main() {
-    printf("hello world");
-    //char t = getchar();
+
+    //test code
+    const char* testdata = "Gimme a string\r\n";
+    fprintf(stdout,testdata);
     
-    //printf("%c",t);
-    getchar();
+    char input[16];
+    fgets(input,sizeof(input), stdin);
+
+    printf("you wrote: %s\r\n",input);
+    printf("strlen: %lli\r\n",strlen(input));
     
+    int count = 0;
+    for (;;count++) {
+        if (getchar() == '\n') {break;}
+    }
+    printf("eaten chars: %i\r\n",count);
+    printf("input size: %lli\r\n",sizeof(input));
+    
+
+    //set console size
+    //write test data to console
+    
+    //initialize console
+    //start game loop
 
     return 0;
 }
